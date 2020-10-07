e9b3aa13e2205e6c4e40f1eb0680e6b18ca2dd97
marco villanueva <villanuevamarco98@gmail.com>
2020-10-06 20:28:05 -0600

    constructor practice contd

--- More_OOP/Program.cs
+++ More_OOP/Program.cs
@@ -7,17 +7,20 @@ namespace More_OOP
         class Car
         {
             public string model;  // Create a field
+            public string color;
 
             // Create a class constructor for the Car class
             public Car()
             {
                 model = "Mustang"; // Set the initial value for model
+                color = "Red";
             }
 
             static void Main(string[] args)
             {
                 Car Ford = new Car();  // Create an object of the Car Class (this will call the constructor)
                 Console.WriteLine(Ford.model);  // Print the value of model
+                Console.WriteLine(Ford.color);
             }
         }
     }

a88cbf51226ee321dfd814dac7d183f9ac0728a1
marco villanueva <villanuevamarco98@gmail.com>
2020-10-06 20:39:17 -0600

    call constructor

--- More_OOP/Program.cs
+++ More_OOP/Program.cs
@@ -9,7 +9,9 @@ namespace More_OOP
             public string model;  // Create a field
             public string color;
 
-            // Create a class constructor for the Car class
+            //Create a class constructor for the Car class
+            //Note that the constructor name must match the class name, 
+            //and it cannot have a return type(like void or int).
             public Car()
             {
                 model = "Mustang"; // Set the initial value for model
@@ -18,6 +20,7 @@ namespace More_OOP
 
             static void Main(string[] args)
             {
+                //Also note that the constructor is called when the object is created new Car.Car().
                 Car Ford = new Car();  // Create an object of the Car Class (this will call the constructor)
                 Console.WriteLine(Ford.model);  // Print the value of model
                 Console.WriteLine(Ford.color);
@@ -25,3 +28,7 @@ namespace More_OOP
         }
     }
 }
+
+
+
+//All classes have constructors by default: if you do not create a class constructor yourself, C# creates one for you. However, then you are not able to set initial values for fields.
